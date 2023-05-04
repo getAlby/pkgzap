7b871be568aea26c018756fb1446ba1c1e8f1add
im-adithya <imadithyavardhan@gmail.com>
2023-05-04 18:59:19 +0530

    chore: remove funding.js

--- funding.js
+++
@@ -1,5 +0,0 @@
-import { getFundingDetails } from "./dist/index.module.js";
-//await getFundingDetails()
-console.log((await getFundingDetails({includeIndirectDeps: true})))
-//console.log((await getFundingDetails({includeIndirectDeps: true})).children.get("@babel/plugin-transform-spread").edgesOut.get("@babel/core").to.package)
-//console.log(await getFundingDetails({includeIndirectDeps: true}))
