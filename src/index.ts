f18c9a984d8bed7d81f37173410799edb2cf6131
im-adithya <imadithyavardhan@gmail.com>
2023-05-05 15:35:58 +0530

    feat: add fetchFundingInfo

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,43 @@ import { readTree } from "./getFunding";
 
 export { cli } from "./bin/index.js";
 
+function allDeps(json) {
+  const deps: string[] = [];
+  if (json.dependencies) {
+    deps.push(...Object.keys(json.dependencies))
+  }
+  if (json.devDependencies) {
+    deps.push(...Object.keys(json.devDependencies))
+  }
+  if (json.peerDependencies) {
+    deps.push(...Object.keys(json.peerDependencies))
+  }
+  if (json.bundleDependencies) {
+    deps.push(...Object.keys(json.bundleDependencies))
+  }
+  if (json.optionalDependencies) {
+    deps.push(...Object.keys(json.optionalDependencies))
+  }
+  return deps;
+}
+
+export async function fetchFundingInfo(json) {
+  const packages = allDeps(json);
+  const fundingInfo = {};
+  for (const packageName of packages) {
+    const url = `https://registry.npmjs.org/${packageName}/latest`;
+    const response = await fetch(url);
+    if (response.ok) {
+      const data = await response.json();
+      const { funding } = data;
+      fundingInfo[packageName] = funding;
+    } else {
+      console.error(`Failed to fetch package info for ${packageName}`);
+    }
+  }
+  return fundingInfo;
+}
+
 export async function getFundingDetails(options?: {includeIndirectDeps: false}) {
   const arborist = new Arborist();
   const tree = await arborist.buildIdealTree();
